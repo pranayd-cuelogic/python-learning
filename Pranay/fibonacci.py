f54151ec9015028f98b127621dbe42fba96b213d
Pranay <pranay@Pranays-Mac-Mini-2.local>
2014-12-24 12:19:56 +0530

    Convert tabs to spaces

--- Pranay/fibonacci.py
+++ Pranay/fibonacci.py
@@ -1,13 +1,13 @@
 def Fibonacci(n):
-	result = []
-	a , b = 0 , 1
-	if(n <= 1):
-		result.append(1)
-		return result
-	while(a < n):
-		result.append(a)
-		a , b = b , a + b
-	return result
+  result = []
+  a , b = 0 , 1
+  if(n <= 1):
+    result.append(1)
+    return result
+  while(a < n):
+    result.append(a)
+    a , b = b , a + b
+  return result
 
 fib = Fibonacci(10)
 
--- Pranay/prime_number.py
+++ Pranay/prime_number.py
@@ -1,10 +1,10 @@
 def GenPrime(x):
-	for m in range(2, x):
-		for n in range(2, m):
-			if  m % n == 0:
-				break
-		else: 
-			print(m, 'is a prime number')
+  for m in range(2, x):
+    for n in range(2, m):
+      if  m % n == 0:
+        break
+    else: 
+      print(m, 'is a prime number')
 
 
 GenPrime(100)
